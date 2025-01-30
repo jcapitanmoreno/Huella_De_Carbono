40ab7f207b89f17e86181af1c4d09da03f51ed1b
jcapitanmoreno <jcapitanmoreno@gmail.com>
2025-01-30 09:27:30 +0100

    correccion de tostring

--- src/main/java/com/github/jcapitanmoreno/services/ActividadService.java
+++ src/main/java/com/github/jcapitanmoreno/services/ActividadService.java
@@ -12,36 +12,8 @@ public class ActividadService {
         actividadDao = new ActividadDao();
     }
 
-    public void addActividad(Actividad actividad) throws Exception {
-        if (actividad.getNombre() == null || actividad.getNombre().isEmpty()) {
-            throw new Exception("El nombre de la actividad no puede estar vacío");
-        }
-        actividadDao.addActividad(actividad);
-    }
-
-    public Actividad getActividad(int id) throws Exception {
-        if (id <= 0) {
-            throw new Exception("El ID de la actividad no es válido");
-        }
-        return actividadDao.getActividad(id);
-    }
-
     public List<Actividad> getAllActividades() {
 
         return  actividadDao.getAllActividades();
-    }
-
-    public void updateActividad(Actividad actividad) throws Exception {
-        if (actividad.getId() <= 0) {
-            throw new Exception("El ID de la actividad no es válido");
-        }
-        actividadDao.updateActividad(actividad);
-    }
-
-    public void deleteActividad(int id) throws Exception {
-        if (id <= 0) {
-            throw new Exception("El ID de la actividad no es válido");
-        }
-        actividadDao.deleteActividad(id);
     }
 }
