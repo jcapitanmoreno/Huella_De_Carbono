329ee1bef10875638adc87686b02f8f557d3db77
jcapitanmoreno <jcapitanmoreno@gmail.com>
2025-02-06 14:53:53 +0100

    camparacion individual terminada (solo queda redimensionar)

--- src/main/java/com/github/jcapitanmoreno/views/ComparacionHuellaController.java
+++ src/main/java/com/github/jcapitanmoreno/views/ComparacionHuellaController.java
@@ -8,10 +8,13 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
 
 import java.math.BigDecimal;
 import java.util.Map;
@@ -21,19 +24,30 @@ public class ComparacionHuellaController {
     private BarChart<String, Number> barChart;
     @FXML
     private ComboBox<String> timePeriodComboBox;
-
+    @FXML
+    private TableView<Impacto> impactTable;
+    @FXML
+    private TableColumn<Impacto, String> actividadColumn;
+    @FXML
+    private TableColumn<Impacto, BigDecimal> impactoColumn;
+    @FXML
+    private Button switchViewButton;
     @FXML
     private ImageView flechaIzquierda;
 
     private HuellaService huellaService;
+    private boolean isBarChartVisible = true;
 
     public void initialize() {
         huellaService = new HuellaService();
         ObservableList<String> timePeriods = FXCollections.observableArrayList("Semana", "Mes", "Año");
         timePeriodComboBox.setItems(timePeriods);
-        timePeriodComboBox.setValue("Semana"); // esto sera lo primero que se setee en el combobox al abrir la pantalla
+        timePeriodComboBox.setValue("Semana");
         timePeriodComboBox.setOnAction(event -> loadComparacion());
         loadComparacion();
+
+        actividadColumn.setCellValueFactory(new PropertyValueFactory<>("actividad"));
+        impactoColumn.setCellValueFactory(new PropertyValueFactory<>("impacto"));
     }
 
     private void loadComparacion() {
@@ -57,20 +71,50 @@ public class ComparacionHuellaController {
         XYChart.Series<String, Number> seriesMedia = new XYChart.Series<>();
         seriesMedia.setName("Media");
 
+        ObservableList<Impacto> impactos = FXCollections.observableArrayList();
+
         for (String categoria : huellaUsuarioPorCategoria.keySet()) {
             BigDecimal huellaUsuario = huellaUsuarioPorCategoria.get(categoria);
             BigDecimal mediaHuella = mediaHuellasPorCategoria.get(categoria);
 
             seriesUsuario.getData().add(new XYChart.Data<>(categoria, huellaUsuario));
             seriesMedia.getData().add(new XYChart.Data<>(categoria, mediaHuella));
+
+            impactos.add(new Impacto(categoria, huellaUsuario));
         }
 
         barChart.getData().addAll(seriesUsuario, seriesMedia);
+        impactTable.setItems(impactos);
+    }
+
+    @FXML
+    private void switchView() {
+        isBarChartVisible = !isBarChartVisible;
+        barChart.setVisible(isBarChartVisible);
+        impactTable.setVisible(!isBarChartVisible);
     }
 
     @FXML
     private void switchToInicioView() {
         Stage stage = (Stage) flechaIzquierda.getScene().getWindow();
         ChangeScene.changeScene(stage, "/com/github/jcapitanmoreno/views/InicioView.fxml");
+    }
+
+    public static class Impacto {
+        private final String actividad;
+        private final BigDecimal impacto;
+
+        public Impacto(String actividad, BigDecimal impacto) {
+            this.actividad = actividad;
+            this.impacto = impacto;
+        }
+
+        public String getActividad() {
+            return actividad;
+        }
+
+        public BigDecimal getImpacto() {
+            return impacto;
+        }
     }
 }
